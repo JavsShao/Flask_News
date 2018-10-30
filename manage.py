08ba594033f0aa42396490d890e793a9c294ce6d
hamdi <javs_shao@163.com>
2018-10-30 10:57:13 +0800

    代码抽取之抽取配置文件

--- manage.py
+++ manage.py
@@ -6,28 +6,9 @@ from flask_session import Session
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
+from config import Config
 
 app = Flask(__name__)
-
-class Config(object):
-    """工程配置信息"""
-    DEBUG = True
-
-    # 数据库的配置信息（MySQL）
-    SQLALCHEMY_DATABASE_URI = "mysql://root:970202@127.0.0.1:3306/Flask_News"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # Redis配置
-    REDIS_HOST = '127.0.0.1'
-    REDIS_PORT = 6379
-
-    SECRET_KEY = "EjpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Yjq52v54g+HVoknA"
-    ...
-    # flask_session的配置信息
-    SESSION_TYPE = "redis"  # 指定 session 保存到 redis 中
-    SESSION_USE_SIGNER = True  # 让 cookie 中的 session_id 被加密签名处理
-    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用 redis 的实例
-    PERMANENT_SESSION_LIFETIME = 86400  # session 的有效期，单位是秒
 
 
 app.config.from_object(Config)
@@ -39,6 +20,7 @@ Session(app)
 manager = Manager(app)
 Migrate(app, db)
 manager.add_command('db', MigrateCommand)
+
 
 @app.route('/index')
 def index():
