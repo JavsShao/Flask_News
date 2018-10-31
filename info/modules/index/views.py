c6d9a984e057df4ac3e1b58b3642536c17c1272e
hamdi <javs_shao@163.com>
2018-10-31 16:17:59 +0800

    title左侧图标

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,8 +1,13 @@
-from flask import render_template
+from flask import render_template, current_app
 
 from . import index_blue
 
 
-@index_blue.route('/index')
+@index_blue.route('/')
 def index():
     return render_template('news/index.html')
+
+@index_blue.route('/favicon.ico', methods=['GET'])
+def favicon():
+    '''title左侧图标'''
+    return current_app.send_static_file('news/favicon.ico')
